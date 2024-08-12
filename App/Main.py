bd77839cc51705f54b48d22941e6b64b037ace14
manika046 <kasulamanika00@gmail.com>
2024-08-12 12:54:32 +0545

    second

--- App/Main.py
+++ App/Main.py
@@ -1,9 +1,11 @@
+import ast
 import time
 import random
 import os
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
@@ -12,9 +14,14 @@ from pathlib import Path
 env_path = Path('../.venv/.env')
 load_dotenv(dotenv_path=env_path)
 
-Firstname = os.getenv("first_name").split(',')
-Lastname = os.getenv("last_name").split(',')
-Password = os.getenv("password").split(',')
+# converting string into python object
+first_names = ast.literal_eval(os.getenv("first_name"))
+last_names = ast.literal_eval(os.getenv("last_name"))
+# passwords = ast.literal_eval(os.getenv("password"))
+
+Firstname = random.choice(first_names)
+Lastname = random.choice(last_names)
+# Password = random.choice(passwords)
 
 driver = webdriver.Chrome()
 
@@ -26,16 +33,53 @@ driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[1]/div/div/div/div[2
 time.sleep(5)
 
 F_name = driver.find_element(By.XPATH, "//input[@name='firstname']")
-F_name.send_keys(random.choice(Firstname))
-time.sleep(5)
+F_name.send_keys(Firstname)
+time.sleep(2)
 
 L_name = driver.find_element(By.XPATH, "//input[@name='lastname']")
-L_name.send_keys(random.choice(Lastname))
-time.sleep(5)
+L_name.send_keys(Lastname)
+time.sleep(2)
+
+MN_element = driver.find_element(By.XPATH, "//input[@name='reg_email__']")
+MN_element.send_keys(os.getenv("Email"))
+time.sleep(2)
+
+# driver.implicitly_wait(10)
+
+MNC_element = driver.find_element(By.XPATH, "//input[@name='reg_email_confirmation__']")
+MNC_element.send_keys(os.getenv("Email"))
+time.sleep(2)
 
 pw_element = driver.find_element(By.XPATH, "//input[@name='reg_passwd__']")
-pw_element.send_keys(random.choice(Password))
+pw_element.send_keys("Hello@123")
 time.sleep(2)
+
+dropdown = Select(driver.find_element(By.ID,"day"))
+dropdown.select_by_value("20")
+
+time.sleep(1)
+
+dropdown = Select(driver.find_element(By.ID,"month"))
+dropdown.select_by_value("10")
+
+time.sleep(1)
+
+dropdown = Select(driver.find_element(By.ID,"year"))
+dropdown.select_by_value("1999")
+time.sleep(1)
+
+radio_button = driver.find_element(By.XPATH, "//input[@value='2']")
+radio_button.click()
+time.sleep(5)
+
+signup_button = driver.find_element(By.NAME, "websubmit")
+signup_button.click()
+time.sleep(10)
+
+print("Account Created Successfully")
+
+driver.switch_to.window(driver.current_window_handle)
+time.sleep(20)
 
 # Create_button = WebDriverWait(driver, 20).until(
 #     EC.element_to_be_clickable((By.XPATH, "//a[@id='u_0_0_lS']"))
